15c9734a93adb2de285099188f42baf9369d332a
arduini-eduarda <132152352+arduini-eduarda@users.noreply.github.com>
2024-06-15 13:19:49 -0300

    Add files via upload

---
+++ exercicios/para-sala/ExercicioParImpar.py
@@ -0,0 +1,9 @@
+#Faça um Programa que leia 10 números inteiros e armazene-os num vetor. 
+#Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
+
+def main():
+    numeros = input("Digite 10 números separados por vírgula: ")
+    
+
+main()
+
---
+++ exercicios/para-sala/ExercicioQuadrado.py
@@ -0,0 +1,10 @@
+# Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos
+# quadrados dos elementos do vetor.
+
+
+def main():
+    numeros = input("Digite 10 números separados por vírgula: ")
+
+
+main()
+
