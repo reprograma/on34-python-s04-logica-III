8b3e571722e92f7e86db7f1f31b47e17ac419fb7
arduini-eduarda <132152352+arduini-eduarda@users.noreply.github.com>
2024-06-22 18:09:34 -0300

    Add files via upload

---
+++ exercicios/para-sala/minhaLoja.py
@@ -0,0 +1,30 @@
+produtos_venda = {
+    "blusa": 10,
+    "calça": 20,
+    "short": 15,
+    "vestido": 30,
+    "sapato": 25
+}
+
+
+#chegou um produto novo, vamos adicionar
+
+
+#acabou o short, precisamos remover
+
+
+#meu cliente comprou uma blusa, quanto ele gastou?
+
+
+#meu cliente adicionou esses itens na compra calca, sapato e vestido, qual o novo valor?
+
+
+#vestido mudou de valor, agora é 45
+
+
+#todos os itens tiveram um aumento de 10 reais
+
+
+#criei um item conjunto, o valor dele é da calca e da blusa somados
+
+
