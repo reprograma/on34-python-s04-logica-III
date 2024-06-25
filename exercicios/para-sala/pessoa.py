e14a0cc33f4ceee530797709ec38b2acc58fb088
arduini-eduarda <132152352+arduini-eduarda@users.noreply.github.com>
2024-06-25 09:26:38 -0300

    Add files via upload

---
+++ exercicios/para-sala/pessoa.py
@@ -0,0 +1,11 @@
+#Crie um dicionário com os pares de chave-valor: "nome": "Ana", "idade": 25.
+
+#Acesse o valor associado à chave "nome".
+
+#Adicione um novo par de chave-valor: "cidade": "São Paulo".
+
+#Verifique se a chave "idade" está no dicionário.
+
+#Crie uma lista de dicionários, onde cada dicionário representa uma pessoa com "nome" e "idade".
+
+#Acesse o nome da segunda pessoa na lista.
