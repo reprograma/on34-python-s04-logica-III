613f47c2171802267001781c23622794a17acad6
Lupaes <luanapaes@yahoo.com.br>
2024-06-27 12:59:29 -0300

    Update Readme.py

--- exercicios/para-casa/Readme.py
+++ exercicios/para-casa/Readme.py
@@ -1,1 +1,71 @@
+#Objetivo 🌍
 
+🛠 Contar a quantidade de vezes que a palavra “lei” ocorre em cada um deles, o que tiver mais ocorrências dessa palavra será o escolhido.
+
+• ⛔Utilizar os conteúdos aprendidos em sala de aula(for, while, listas/tuplas/dicionários/conjuntos e suas funções).
+• ⛔⛔Na resposta deverá ser impresso quais dos textos foi escolhido.
+
+#Recursos 👩🏽‍💻
+
+•	🎓 Professora: Aula de revisão 
+•	🛠 Classrrom: Repositórios de todas as aulas 
+•	💻 Videos das aulas: Youtube 
+•	👩🏽‍🤝‍👩🏻 Grupo do Whatsapp: Nenhuma solta a mão da outra 
+•	🤖 Colab:https://colab.research.google.com/ 
+
+#Desenvolvimento do código
+
+##Estratégia escolhida🎯
+• Identificar separadamente quantas vezes a palavra LEI aparece em cada texto.
+• Identificado a quantidade criar um dicionário chamado lista_textos para realização da atividade de casa sugerida pela Professora.
+• Utilização da plataforma do Colab no Googogle, devido a problemas técnicos da minha máquina( orientação dada pela LT)
+
+##Passo 1: 👣
+•
+####Utilização da função SPLIT
+•
+• O objetivo da utilização dessa função é contabilizar o números de palavras em cada texto.
+
+##Passo 2👣
+•
+###Utilização da função COUNT
+•
+• O objetivo da utilização dessa função é contabilizar quantas vezes a LEI aparece no conjunto de palavras de cada texto já identificado.
+•
+##Passo 3👣
+•
+• Criação do dicionário lista_textos
+•
+##Passo 4👣
+•
+• Criação do dicionário lista_textos
+•
+##Passo 5👣
+•
+####Utilização do FOR e IN
+•
+• O objetivo da utilização é para identificar as variáveis de cada item do dicionário criado.
+•
+##Passo 6👣
+•
+###Utilização da função Q
+•
+• O objetivo da utilização é para identificar o texto com a maior quantidade da palavra LEI.
+•
+##Passo 7👣
+•
+• Printar o texto escolhido.
+•
+##Passo 8👣
+•
+• Código concluído.
+•
+#Comentários finais 💪🏽 
+  
+• Achei bem desafiador esse exercício.🔥 
+• O resultado foi muito legal, foi muito emocionante ver o código rodar, porém próximo do que eu almejava, visto que ao rodar o código a resposta se repetia.
+•
+• O código foi todo desenvolvido pela plataforma do Colab e se encontra na pasta por mim criada Link do Colab.
+•  
+• Adorei ❤️ 
+• Feito com ❤️ por Luana Paes
