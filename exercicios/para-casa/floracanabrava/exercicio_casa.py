6f40b5588fa86240861896b81714a7ff6732f259
floracanabrava <floracanabrava@gmail.com>
2024-06-28 21:49:24 -0300

    atualização do código do exercicio da semana 04

--- exercicios/para-casa/floracanabrava/exercicio_casa.py
+++ exercicios/para-casa/floracanabrava/exercicio_casa.py
@@ -3,7 +3,7 @@ def main():
     texto1 = "Uma infecção curada de dengue confere ao paciente imunidade contra o tipo de vírus responsável. Por existirem quatro tipos diferentes de vírus, para estar totalmente imunizado, é necessário ter tido contato com todos eles. Caso contrário, a cada contágio com um novo tipo de vírus, os sintomas são mais intensos e o risco de desenvolver a dengue grave é mais alto."
     texto2 = "Já num sentido amplo, lei é somente a regra jurídica escrita, excluindo-se dessa aceção, portanto, o costume jurídico. Por fim, numa aceção técnica e específica, a palavra lei designa uma modalidade de regra escrita, que apresenta determinadas características; no direito brasileiro, são técnicas apenas a lei complementar e a lei ordinária."
     texto3 = "Na linha do tempo do futebol feminino brasileiro, ele passou tanto tempo proibido quanto regulamentado: 38 anos. De 1941 a 1979, a lei nacional não permitia a prática do esporte por mulheres devido às “condições da sua natureza”. Já o regulamento da modalidade, que permanece válido até 2021, só foi feito em 1983."
-    texto4 = "A lei dos cossenos é uma expressão matemática que relaciona os três lados de um triângulo qualquer. Por não estar restrita ao triângulo retângulo, a lei dos cossenos pode ser entendida como uma generalização do teorema de Pitágoras."
+    texto4 = "A lei brasileira brasileira brasileira brasileira dos cossenos é uma expressão matemática que relaciona os três lados de um triângulo qualquer. Por não estar restrita ao triângulo retângulo, a lei dos cossenos pode ser entendida como uma generalização do teorema de Pitágoras."
     
     textos = [texto1, texto2, texto3, texto4]
 
@@ -11,7 +11,8 @@ def main():
 
     qt_lei_textos = []
     for texto in textos:
-        qt_lei_texto = texto.count("lei")
+        palavras = texto.split()
+        qt_lei_texto = palavras.count("lei")
         qt_lei_textos.append(qt_lei_texto)
 
     maior = max(qt_lei_textos)
